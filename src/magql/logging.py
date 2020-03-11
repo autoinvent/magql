49292403bb4a633fa4359c89fd25f30bf9a28c04
Kyler Ramsey <kyler.ramsey@moesol.com>
2020-03-11 11:25:47 -0700

    remove default file creation from logger

--- src/magql/logging.py
+++ src/magql/logging.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-from logging import FileHandler
 
 FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 LOG_FILE = "magql.log"
@@ -12,17 +11,10 @@ def get_console_handler():
     return console_handler
 
 
-def get_file_handler():
-    file_handler = FileHandler(filename=LOG_FILE)
-    file_handler.setFormatter(FORMATTER)
-    return file_handler
-
-
 def get_logger(logger_name):
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.ERROR)
     logger.addHandler(get_console_handler())
-    logger.addHandler(get_file_handler())
     logger.propagate = False
 
     return logger
