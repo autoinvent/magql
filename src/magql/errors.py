74e02ad4909e5938de4682ee94cede2e7b97cc88
Kyler Ramsey <kyler.ramsey@moesol.com>
2020-03-11 08:50:45 -0700

    add authorization error, rename validator.py to errors.py

--- src/magql/errors.py
+++ src/magql/errors.py
@@ -3,3 +3,10 @@ class ValidationFailedError(Exception):
         if not isinstance(errors, list):
             errors = [errors]
         self.errors = errors
+
+
+class AuthorizationError(Exception):
+    def __init__(self, errors):
+        if not isinstance(errors, list):
+            errors = [errors]
+        self.errors = errors
